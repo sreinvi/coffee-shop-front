4d07b5effa9dc5cffff7e1188d0698d0debc21d0
darkheart101 <kurokokoro@windowslive.com>
2020-09-17 06:19:38 +0300

    feature: item modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { ItemsComponent } from './items/items.component';
 import {RouterModule} from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import {HttpClientModule} from '@angular/common/http';
+import {ItemModalComponent, ItemModalContent} from './modals/item-modal/item-modal-component';
 
 
 @NgModule({
@@ -13,10 +14,13 @@ import {HttpClientModule} from '@angular/common/http';
     AppComponent,
     ItemsComponent,
     HeaderComponent,
+    ItemModalComponent,
+    ItemModalContent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
+    NgbModule,
     RouterModule.forRoot([
       {path: 'items', component: ItemsComponent},
     ]),
---
+++ src/app/modals/item-modal/item-modal-component.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'item-modal-content',
+  templateUrl: './item-modal-content.html'
+})
+export class ItemModalContent {
+
+  // tweetPayload: TweetPayload;
+  // addTweetForm: FormGroup;
+  // tweet = new FormControl('');
+
+  constructor(public activeModal: NgbActiveModal, private router: Router)
+  {
+    // this.addTweetForm = new FormGroup({
+    //   tweet: this.tweet,
+    // });
+
+    // this.tweetPayload = {
+    //   tweet_id: null,
+    //   tweet_message: '',
+    //   tweet_user_id: null
+    // }
+  }
+
+  // loadData(tweetid:number)
+  // {
+  //   this.tweetService.getTweet(tweetid).subscribe( (data:SingleTweet) =>{
+  //     this.tweetPayload.tweet_id = data.data.tweet_id;
+  //     this.addTweetForm.controls.tweet.setValue(data.data.tweet_message);
+  //   },(error:any)=>{
+  //     alert(error);
+  //   });
+  // }
+
+  update()
+  {
+    // this.tweetPayload.tweet_message = this.addTweetForm.get('tweet').value;
+    // this.tweetService.updateTweet(this.tweetPayload).subscribe( data=>{
+    //   window.location.reload();
+    // },error =>{
+    //   alert(error);
+    // });
+  }
+
+  save(){
+    // if(this.tweetPayload.tweet_id > 0){
+    //   this.update();
+    //   return;
+    // }
+    // this.tweetPayload.tweet_message = this.addTweetForm.get('tweet').value;
+    // this.tweetService.newTweet(this.tweetPayload).subscribe( data=>{
+    //   window.location.reload();
+    // }, error => {
+    //   alert(error);
+    // });
+  }
+
+}
+
+@Component({
+  selector: 'item-modal',
+  templateUrl: './tweet-modal-component.html'
+})
+export class ItemModalComponent {
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(ItemModalContent);
+  }
+
+
+}
