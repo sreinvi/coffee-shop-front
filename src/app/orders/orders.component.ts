bc1649162b384d3ddadc109f8f250374d5aa7b9c
darkheart101 <kurokokoro@windowslive.com>
2020-10-29 06:42:54 +0200

    refactor: delete unused inports

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {OrdersService} from '../services/orders.service';
-import {TableResponse} from '../dto/responses/TableResponse';
 
 @Component({
   selector: 'app-orders',
--- src/app/services/orders.service.ts
+++ src/app/services/orders.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {AppConfig} from '../config/app-config';
 import {HttpClient} from '@angular/common/http';
-import {TableResponse} from '../dto/responses/TableResponse';
 
 @Injectable({
   providedIn: 'root'
